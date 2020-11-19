556cd9255d9e8e799ebba0034fadfa61ee2382ed
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-11-19 03:56:08 +0300

    Add convertor currancy

--- markups.py
+++ markups.py
@@ -12,12 +12,12 @@ def first_markup():
 
 
 def second_markup():
-    course_markup_btn1 = KeyboardButton('RUS → USD')
-    course_markup_btn2 = KeyboardButton('RUS → EUR')
-    course_markup_btn3 = KeyboardButton('RUS → CHF')
-    course_markup_btn4 = KeyboardButton('RUS → TRY')
-    course_markup_btn5 = KeyboardButton('RUS → JPY')
-    course_markup_btn6 = KeyboardButton('RUS → BTC')
+    course_markup_btn1 = KeyboardButton('RUB → USD')
+    course_markup_btn2 = KeyboardButton('RUB → EUR')
+    course_markup_btn3 = KeyboardButton('RUB → CHF')
+    course_markup_btn4 = KeyboardButton('RUB → TRY')
+    course_markup_btn5 = KeyboardButton('RUB → JPY')
+    course_markup_btn6 = KeyboardButton('RUB → BTC')
     course_markup = ReplyKeyboardMarkup([[course_markup_btn1, course_markup_btn2, course_markup_btn3],
                                          [course_markup_btn4, course_markup_btn5, course_markup_btn6],
                                          ['↺', 'Назад']], resize_keyboard=True)
@@ -25,12 +25,12 @@ def second_markup():
 
 
 def re_second_markup():
-    rewrite_course_btn1 = KeyboardButton('USD → RUS')
-    rewrite_course_btn2 = KeyboardButton('EUR → RUS')
-    rewrite_course_btn3 = KeyboardButton('CHF → RUS')
-    rewrite_course_btn4 = KeyboardButton('TRY → RUS')
-    rewrite_course_btn5 = KeyboardButton('JPY → RUS')
-    rewrite_course_btn6 = KeyboardButton('BTC → RUS')
+    rewrite_course_btn1 = KeyboardButton('USD → RUB')
+    rewrite_course_btn2 = KeyboardButton('EUR → RUB')
+    rewrite_course_btn3 = KeyboardButton('CHF → RUB')
+    rewrite_course_btn4 = KeyboardButton('TRY → RUB')
+    rewrite_course_btn5 = KeyboardButton('JPY → RUB')
+    rewrite_course_btn6 = KeyboardButton('BTC → RUB')
     rewrite_course = ReplyKeyboardMarkup([[rewrite_course_btn1, rewrite_course_btn2, rewrite_course_btn3],
                                          [rewrite_course_btn4, rewrite_course_btn5, rewrite_course_btn6],
                                          ['↺', 'Назад']], resize_keyboard=True)
